3aaa93c18c75bbde98d6dae691937aef9c60099c
Unknown <solariousz@gmail.com>
2021-12-06 16:51:05 +0800

    Added slash command to mock a users last message

--- mock-bot.js
+++ mock-bot.js
@@ -17,8 +17,9 @@ export async function createMockBot() {
 
     client.once('ready', async () => {
         console.log('Setting up mock bot');
-        await setupMockUserCommand();
+        //await setupMockUserCommand();
         await setupMockMessageCommand();
+        await setupMockUsersLastMessageCommand();
         console.log('Mock bot ready');
     });
 
@@ -26,6 +27,8 @@ export async function createMockBot() {
         if (interaction.isCommand()) {
             if (interaction.commandName === 'mockuser') {
                 await mockUserInteraction(interaction);
+            } else if (interaction.commandName === 'mockuserslastmessage') {
+                await mockUsersLastMessageInteraction(interaction);
             }
         } else if (interaction.isApplicationCommand()) {
             if (interaction.commandName === 'mockmessage') {
@@ -86,6 +89,38 @@ async function mockMessageInteraction(interaction) {
     const message = interaction.channel.messages.cache.get(interaction.targetId).content;
     const mockedMessage = mockify(message);
     await interaction.reply(mockedMessage);
+}
+
+async function setupMockUsersLastMessageCommand() {
+    const json = {
+        'name': 'mockuserslastmessage',
+        'type': 1,
+        'description': 'Mocks the given users last message in this channel',
+        'options': [
+            {
+                'name': 'user',
+                'type': 6,
+                'description': 'The user to mock'
+            }
+        ]
+    };
+    
+    await setupCommand(MOCK_TOKEN, MOCK_CLIENT_ID, GUILD_ID, json);
+}
+
+async function mockUsersLastMessageInteraction(interaction) {
+    const messages = await interaction.channel.messages.fetch();
+    const usersMessages = messages.filter(m => m.author.id == interaction.options.getUser('user').id);
+    if (usersMessages.size == 0) {
+        await interaction.reply({
+            contents: 'Could not find a message to mock',
+            ephemeral: true
+        });
+    } else {
+        const message = usersMessages.first().content;
+        const mocked = mockify(message);
+        await interaction.reply(mocked);
+    }
 }
 
 function mockify(text) {
